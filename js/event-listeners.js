1d5cc0a70b015c9bc0aef212ab524eef2af5525c
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2024-07-22 10:58:30 +0200

    Create event-listeners.js

---
+++ js/event-listeners.js
@@ -0,0 +1,35 @@
+function autoSubmitByLength()
+{
+    if (piGetValue('otp').length === parseInt(piGetValue("autoSubmitOtpLength")))
+    {
+        document.forms["piLoginForm"].submit();
+    }
+}
+
+function eventListeners()
+{
+    if (piGetValue("activateAutoSubmitOtpLength"))
+    {
+        document.getElementById("otp").addEventListener("keyup", autoSubmitByLength);
+    }
+
+    // BUTTON LISTENERS
+    document.getElementById("webAuthnButton").addEventListener("click", function ()
+    {
+        piChangeMode("webauthn");
+    });
+    document.getElementById("pushButton").addEventListener("click", function ()
+    {
+        piChangeMode("push");
+    });
+    document.getElementById("otpButton").addEventListener("click", function ()
+    {
+        piChangeMode("otp");
+    });
+}
+
+// Wait until the document is ready
+document.addEventListener("DOMContentLoaded", function ()
+{
+    eventListeners();
+});
