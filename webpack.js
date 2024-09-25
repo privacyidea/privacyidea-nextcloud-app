514ff27890f59cf3106cd109083a3e4c00c83b94
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2024-09-25 11:45:23 +0200

    update to v30

--- webpack.js
+++ webpack.js
@@ -1,6 +1,4 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
-const ESLintPlugin = require('eslint-webpack-plugin')
-const StyleLintPlugin = require('stylelint-webpack-plugin')
 const path = require('path')
 
 webpackConfig.entry = {
@@ -11,22 +9,5 @@ webpackConfig.entry = {
 	pollTransactionWorker: { import: path.join(__dirname, 'js', 'pollTransaction.worker.js'), filename: 'pollTransaction.worker.js' },
 	settingsAdmin: { import: path.join(__dirname, 'js', 'settings-admin.js'), filename: 'settings-admin.js' },
 }
-
-webpackConfig.plugins.push(
-	new ESLintPlugin({
-		extensions: ['js', 'vue'],
-		files: 'src',
-	}),
-)
-webpackConfig.plugins.push(
-	new StyleLintPlugin({
-		files: 'src/**/*.{css,scss,vue}',
-	}),
-)
-
-webpackConfig.module.rules.push({
-	test: /\.svg$/i,
-	type: 'asset/source',
-})
 
 module.exports = webpackConfig
