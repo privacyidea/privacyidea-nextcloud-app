0be73ebb837807645de402f149fb4b1f80b847c1
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2024-10-02 14:29:42 +0200

    Update PrivacyIDEAProvider.php

--- lib/Provider/PrivacyIDEAProvider.php
+++ lib/Provider/PrivacyIDEAProvider.php
@@ -371,14 +371,26 @@ class PrivacyIDEAProvider implements IProvider
         {
             $pi = new PrivacyIDEA("privacyidea-nextcloud/1.0.0", $this->getAppValue("piURL", ""));
             $pi->setLogger($this->logger);
-            $pi->setSSLVerifyHost($this->getAppValue("piSSLVerify", "true"));
-            $pi->setSSLVerifyPeer($this->getAppValue("piSSLVerify", "true"));
+            $pi->setSSLVerifyHost($this->getAppValue("piSSLVerify", true));
+            $pi->setSSLVerifyPeer($this->getAppValue("piSSLVerify", true));
             $pi->setServiceAccountName($this->getAppValue("piServiceName", ""));
             $pi->setServiceAccountPass($this->getAppValue("piServicePass", ""));
             $pi->setServiceAccountRealm($this->getAppValue("piServiceRealm", ""));
             $pi->setRealm($this->getAppValue("piRealm", ""));
-            $pi->setForwardClientIP($this->getAppValue("piForwardClientIP", false));
             $pi->setNoProxy($this->getAppValue("piNoProxy", false));
+            $pi->setForwardClientIP($this->getAppValue("piForwardClientIP", false));
+            if ($this->getAppValue("piForwardClientIP", false))
+            {
+                $clientIP = $this->getClientIP();
+                if (!empty($clientIP))
+                {
+                    $pi->setClientIP($clientIP);
+                }
+                else
+                {
+                    $this->log("error", "Forward client IP: Cannot get client IP address.");
+                }
+            }
             return $pi;
         }
         else
@@ -613,21 +625,14 @@ class PrivacyIDEAProvider implements IProvider
     /**
      * Get the client IP address.
      *
-     * @return mixed|string
+     * @return string the client IP address or an empty string if not available.
      */
-    public function getClientIP(): mixed
+    public function getClientIP(): string
     {
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER))
+        $clientIP = $this->request->getRemoteAddress();
+        if (!empty($clientIP))
         {
-            return $_SERVER["HTTP_X_FORWARDED_FOR"];
-        }
-        else if (array_key_exists('REMOTE_ADDR', $_SERVER))
-        {
-            return $_SERVER["REMOTE_ADDR"];
-        }
-        else if (array_key_exists('HTTP_CLIENT_IP', $_SERVER))
-        {
-            return $_SERVER["HTTP_CLIENT_IP"];
+            return $clientIP;
         }
         return '';
     }
